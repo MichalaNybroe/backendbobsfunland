9a0b489917bcb3ed5eb60a06539a5a8fa4e80eaf
Michala Nybroe <michalanim@gmail.com>
2022-03-14 12:03:02 +0100

    Initial commit

---
+++ src/main/java/com/example/backendbobsfunland/BackendbobsfunlandApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendbobsfunland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendbobsfunlandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendbobsfunlandApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backendbobsfunland/BackendbobsfunlandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendbobsfunland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendbobsfunlandApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
