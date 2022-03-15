7a60cb92362d653ff3fb6f0acc7a94fb3b251e08
kamillenikolajsen <knikolajsen4@gmail.com>
2022-03-15 14:08:52 +0100

    Add LoginRESTController and changes in instructorRep.

---
+++ src/main/java/com/example/backendbobsfunland/RESTcontroller/LoginRESTController.java
@@ -0,0 +1,36 @@
+package com.example.backendbobsfunland.RESTcontroller;
+
+import com.example.backendbobsfunland.model.Instructor;
+import com.example.backendbobsfunland.repository.InstructorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@CrossOrigin
+public class LoginRESTController {
+
+  @Autowired
+  InstructorRepository instructorRepository;
+
+  @PostMapping("/login")
+  public void login(HttpSession session, HttpServletResponse response, @RequestBody Instructor instructor) throws IOException {
+
+    Instructor inst = instructorRepository.findByName("email");
+
+     if(inst.getEmail().equals(instructor.getEmail()) && inst.getPassword().equals(instructor.getPassword())){
+
+       session.setAttribute("email", inst.getEmail());
+
+       response.sendRedirect("/index");
+     }
+  }
+
+
+
+}
