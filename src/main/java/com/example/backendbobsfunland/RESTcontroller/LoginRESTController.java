ccc54a2a6f10213afed99084666630409f169343
kamillenikolajsen <knikolajsen4@gmail.com>
2022-03-17 10:23:01 +0100

    Added changes in Login and added indexRESTController

--- src/main/java/com/example/backendbobsfunland/RESTcontroller/LoginRESTController.java
+++ src/main/java/com/example/backendbobsfunland/RESTcontroller/LoginRESTController.java
@@ -3,32 +3,40 @@ package com.example.backendbobsfunland.RESTcontroller;
 import com.example.backendbobsfunland.model.Instructor;
 import com.example.backendbobsfunland.repository.InstructorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.util.List;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:63342", allowedHeaders = "*")
 public class LoginRESTController {
 
   @Autowired
   InstructorRepository instructorRepository;
 
+  @CrossOrigin(origins = "http://localhost:63342", allowedHeaders = "*")
   @PostMapping("/login")
   public void login(HttpSession session, HttpServletResponse response, @RequestBody Instructor instructor) throws IOException {
-
+    //response.sendRedirect("/index");
     Instructor inst = instructorRepository.findByName("email");
 
-     if(inst.getEmail().equals(instructor.getEmail()) && inst.getPassword().equals(instructor.getPassword())){
+    //System.out.println(inst);
 
-       session.setAttribute("email", inst.getEmail());
+    //return instructorRepository.findByName("email");
+
+    if(inst.getEmail().equals(instructor.getEmail()) && inst.getPassword().equals(instructor.getPassword())){
+
+       //session.setAttribute("email", inst.getEmail());
 
        response.sendRedirect("/index");
+
+
      }
+
+
+
   }
 
 
