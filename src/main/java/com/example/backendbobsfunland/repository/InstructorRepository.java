b7a4688d6c950360fc92992fe43eae155b97aa1d
MichalaNybroe <82261201+MichalaNybroe@users.noreply.github.com>
2022-03-17 09:28:58 +0100

    Revert "Add LoginRESTController and changes in instructorRep."

--- src/main/java/com/example/backendbobsfunland/repository/InstructorRepository.java
+++ src/main/java/com/example/backendbobsfunland/repository/InstructorRepository.java
@@ -4,6 +4,4 @@ import com.example.backendbobsfunland.model.Instructor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InstructorRepository extends JpaRepository<Instructor,String> {
-
-  Instructor findByName(String name);
 }
