3e65d6de2955070847e5245e71079856fe2f1e83
Rasmus-Kibshede <2740Ralle@gmail.com>
2022-03-23 15:48:52 +0100

    auto send email after deleting

--- src/main/java/com/example/backendbobsfunland/RESTcontroller/BookingRESTController.java
+++ src/main/java/com/example/backendbobsfunland/RESTcontroller/BookingRESTController.java
@@ -9,7 +9,11 @@ import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import java.util.List;
+import java.util.Properties;
 
 @RestController
 @CrossOrigin
@@ -51,8 +55,60 @@ public class BookingRESTController {
 
         Booking b = bookingRepository.findByOrderNumber(booking.getOrderNumber());
 
-        if (b == null){
-            System.out.println("There is no booking with that order number");
+        if (b == null) {
+            sendEmail(booking.getCustomer().getEmail());
+        }
+    }
+
+    public void sendEmail(String to) {
+        // Sender's email ID needs to be mentioned
+        String from = System.getenv("email");
+
+        // Assuming you are sending email from through gmails smtp
+        String host = "smtp.gmail.com";
+
+        // Get system properties
+        Properties properties = System.getProperties();
+
+        // Setup mail server
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.port", "465");
+        properties.put("mail.smtp.ssl.enable", "true");
+        properties.put("mail.smtp.auth", "true");
+
+        // Get the Session object.// and pass username and password
+        Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(from, System.getenv("mailPassword"));
+            }
+        });
+
+        // Used to debug SMTP issues
+        session.setDebug(true);
+
+        try {
+            // Create a default MimeMessage object.
+            MimeMessage message = new MimeMessage(session);
+
+            // Set From: header field of the header.
+            message.setFrom(new InternetAddress(from));
+
+            // Set To: header field of the header.
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+            // Set Subject: header field
+            message.setSubject("This is the Subject Line!");
+
+            // Now set the actual message
+            message.setText("This is actual message");
+
+            System.out.println("sending...");
+            // Send message
+            Transport.send(message);
+            System.out.println("Sent message successfully....");
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
         }
     }
 }
