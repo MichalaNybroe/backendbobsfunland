adab92e67b179d051f107bd2f8c481051a81cc19
Michala Nybroe <michalanim@gmail.com>
2022-03-24 10:38:42 +0100

    removed outcomments in top of restcontrollerbooking

--- src/main/java/com/example/backendbobsfunland/RESTcontroller/BookingRESTController.java
+++ src/main/java/com/example/backendbobsfunland/RESTcontroller/BookingRESTController.java
@@ -6,13 +6,10 @@ import com.example.backendbobsfunland.repository.BookingRepository;
 import com.example.backendbobsfunland.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @CrossOrigin
